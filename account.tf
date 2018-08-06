990258ccfd88a903fbe9d07cfe216eaff4db352f
Craig R Webster <craig@barkingiguana.com>
2018-08-06 09:19:03 +0800

    Alias needs to be run with provider inside account
    This was re-aliasing the master account. Oops.

--- account.tf
+++ account.tf
@@ -1,15 +1,10 @@
 locals {
   email = "${var.email == "calculated" ? "craig+${var.organisation}_${var.application}_${var.environment}@barkingiguana.com" : var.email}"
   name  = "${var.name == "calculated" ? "${var.organisation}_${var.application}_${var.environment}" : var.name}"
-  account_alias  = "${var.account_alias == "calculated" ? "${var.organisation}-${var.application}-${var.environment}" : var.account_alias}"
 }
 
 resource "aws_organizations_account" "account" {
   name      = "${local.name}"
   email     = "${local.email}"
   role_name = "${var.role_name}"
-}
-
-resource "aws_iam_account_alias" "alias" {
-  account_alias = "${local.account_alias}"
 }
